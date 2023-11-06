7b954e3d60911e9d0e097fa47f3f7d24e4de5dc2
Trung Tín Võ <votrungtin@Tins-MacBook-Pro.local>
2023-11-06 17:23:39 +0700

    Change to mysql

--- src/app.js
+++ src/app.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const app = express();
 const PORT = "6969";
-//const adminRoute = require("./routes/adminRoute");
 const staffRoute = require("./routes/staffRoute");
 const dentistRoute = require("./routes/dentistRoute");
 const patientRoute = require("./routes/patientRoute");
 const bodyParser = require("body-parser");
-//const methodOverride = require('method-override')
 
 app.use('/public', express.static('public'));
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(methodOverride('_method'));
 app.get('/', (req, res) => {
   res.render('login')
 })
