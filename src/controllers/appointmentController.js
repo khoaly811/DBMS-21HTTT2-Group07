747f6794bd97682fb84f609e039847b60c7a7425
Trung Tín Võ <votrungtin@Tins-MacBook-Pro.local>
2023-11-13 15:31:58 +0700

    Fix frontend

--- src/controllers/appointmentController.js
+++ src/controllers/appointmentController.js
@@ -1,12 +1,10 @@
 const Appointment = require('../models/appointments');
 
-
 exports.listAppointment = function(req, res) {
-    Appointment.listAppointment(function(err, appointment) {
-      if (err)
-      res.send(err);
-      console.log('res', appointment);
-      res.render('bookAppointment', {appointment})
-    });
+  Appointment.listAppointment(function(err, appointment) {
+    if (err)
+    res.send(err);
+    console.log('res', appointment);
+    res.render('bookAppointment', {appointment: appointment})
+  });
 };
- 
