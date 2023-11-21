5cf91e7522d6e61cc1309fc05713848c1a3da8b5
Albert de Nafta <treblaatfan1406@gmail.com>
2023-11-21 18:07:08 +0700

    GG

--- src/models/invoices.js
+++ src/models/invoices.js
@@ -13,37 +13,39 @@ const Invoice = function(invoice){
 Invoice.invoiceList = function(req, res, next) {
     const patientID = req.params.id;
   
-    // USE `adb_nhakhoa`;
-    // DELIMITER $$
-    
-    // CREATE PROCEDURE sp_getInvoiceByPatientID(IN patient_id VARCHAR(9))
-    // BEGIN
-    //     DECLARE treatment_id_var VARCHAR(9);
-    
-    //     -- Get Treatment_ID based on Patient_ID
-    //     SELECT 
-    //         TREATMENT_ID INTO treatment_id_var
-    //     FROM 
-    //         TREATMENT
-    //     WHERE 
-    //         PATIENT_ID = patient_id;
-    
-    //     -- Check if Treatment_ID is not null
-    //     IF treatment_id_var IS NOT NULL THEN
-    //         -- Get Invoice based on Treatment_ID
-    //         SELECT 
-    //             *
-    //         FROM 
-    //             INVOICE
-    //         WHERE 
-    //             TREATMENT_ID = treatment_id_var;
-    //     ELSE
-    //         -- If Treatment_ID is null, return a message or handle as needed
-    //         SELECT 'No treatment found for the given patient.' AS Message;
-    //     END IF;
-    // END $$
-    
-    // DELIMITER ;
+//     USE `adb_nhakhoa`;
+// DELIMITER $$
+
+// CREATE PROCEDURE sp_getInvoiceByPatientID(IN patient_id VARCHAR(9))
+// BEGIN
+//     DECLARE treatment_id_var VARCHAR(9);
+
+//     -- Get Treatment_ID based on Patient_ID
+//     SELECT 
+//         TREATMENT_ID INTO treatment_id_var
+//     FROM 
+//         TREATMENT
+//     WHERE 
+//         PATIENT_ID = patient_id
+//     LIMIT 1;
+
+//     -- Check if Treatment_ID is not null
+//     IF treatment_id_var IS NOT NULL THEN
+//         -- Get Invoice based on Treatment_ID
+//         SELECT 
+//             *
+//         FROM 
+//             INVOICE
+//         WHERE 
+//             TREATMENT_ID = treatment_id_var;
+//     ELSE
+//         -- If Treatment_ID is null, return a message or handle as needed
+//         SELECT 'No treatment found for the given patient.' AS Message;
+//     END IF;
+// END $$
+
+// DELIMITER ;
+
     let sql= `call sp_getInvoiceByPatientID('${patientID}')`;
     
     db.query(sql, function(err, invoiceList) {
