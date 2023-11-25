88196499812dcdcbc943f28ab6190979c36782bf
tgen0123 <100363182+tgen0123@users.noreply.github.com>
2023-11-26 02:07:22 +0700

    Update authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,24 +1,32 @@
 const Account = require("../models/accounts");
-const { extractUsernameAndRole } = require('./accounts');
+const { extractUsernameAndRole } = require('../models/accounts');
+const { getAllUsernames } = require('../models/accounts');
 
 exports.roleAuth = async (req, res) => {
   const username = req.body.username;
-  const password = 12345678;console.log(username, password);
+
+  const password = "12345678";
+  const usernames = await Account.getAllUsernames();
+
+  console.log(username);
+
   try {
-    const account = await Account.findOneByUsername(username);
+    const account = await Account.findOneByUsernameFromArray(usernames, username);
+
     if (account) {
       // Check the password here (use a proper authentication mechanism)
       if (password) {
         req.session.user = extractUsernameAndRole(account);
+
         switch (req.session.user.role) {
           case 'admin':
             res.redirect('/admin');
             break;
           case 'dentist':
-            res.redirect('/patient');
+            res.redirect('/patientList');
             break;
           case 'staff':
-            res.redirect('/staff');
+            res.redirect('/patientList');
             break;
           case 'patient':
             res.redirect('/patientList');
@@ -26,14 +34,15 @@ exports.roleAuth = async (req, res) => {
           default:
             res.status(401).send('Invalid role');
         }
-    } else {
-            res.status(401).send('Username or password is incorrect');
-          }
-        } else {
-          res.status(401).send('Username or password is incorrect');
-        }
-      } catch (err) {
-        console.error('From auth', err);
-        res.status(400).send(err.message);
+      } else {
+        res.status(401).send('Password is incorrect');
       }
-    };
+    } else {
+      res.status(401).send('Username is undefined');
+    }
+  } catch (err) {
+    console.error('From auth', err);
+    res.status(400).send(err.message);
+  }
+};
+
