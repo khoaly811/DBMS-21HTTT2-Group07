684b95aa7d911490bc3d5ebb2cb89755bd6f0b2c
tgen0123 <100363182+tgen0123@users.noreply.github.com>
2023-11-26 00:33:27 +0700

    Update authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,48 +1,39 @@
 const Account = require("../models/accounts");
+const { extractUsernameAndRole } = require('./accounts');
 
 exports.roleAuth = async (req, res) => {
-    const username = req.body.username;
-    const password = 12345678;
-    console.log(username, password);
-
-    Account
-    .findOne({ username: username})
-    .then(account => {
-        if (account) {
-            req.session.user = {};
-            req.session.user.username = account.username;
-            // res.set('Set-Cookie', 'sessionID=' + req.sessionID);
-            const firstTwoWords = account.username.split(' ').slice(0, 2);
-            
-            if (firstTwoWords === 'ADM') {
-                // req.session.user.role = 'dentist';
-                res.redirect('/admin');
-            }
-            else if (firstTwoWords === 'DEN') {
-                // req.session.user.role = 'dentist';
-                res.redirect('/dentist');
-            }
-            // else if (account.username === '') {
-            //     req.session.user.role = 'staff';
-            //     res.redirect('/staff');
-            // }
-            else if  (firstTwoWords === 'ST') {
-                // req.session.user.role = 'dentist';
-                res.redirect('/staff');
-            }
-            else {
-                //req.session.user.role = 'patient';
-                res.redirect('/patient');
-                // console.log(req.sessionID);
-                // res.send('Logged in');
-            }
+  const username = req.body.username;
+  const password = 12345678;console.log(username, password);
+  try {
+    const account = await Account.findOneByUsername(username);
+    if (account) {
+      // Check the password here (use a proper authentication mechanism)
+      if (password) {
+        req.session.user = extractUsernameAndRole(account);
+        switch (req.session.user.role) {
+          case 'admin':
+            res.redirect('/admin');
+            break;
+          case 'dentist':
+            res.redirect('/patient');
+            break;
+          case 'staff':
+            res.redirect('/staff');
+            break;
+          case 'patient':
+            res.redirect('/patientList');
+            break;
+          default:
+            res.status(401).send('Invalid role');
         }
-        else {
-            throw new Error('Username or password is incorrect');
+    } else {
+            res.status(401).send('Username or password is incorrect');
+          }
+        } else {
+          res.status(401).send('Username or password is incorrect');
         }
-    })
-    .catch(err => {
+      } catch (err) {
         console.error('From auth', err);
         res.status(400).send(err.message);
-    })
-}
+      }
+    };
