30b964e5085af5b2e7941ff0280fea2f4fddbb0e
tgen0123 <100363182+tgen0123@users.noreply.github.com>
2023-11-26 00:33:23 +0700

    Update accounts.js

--- src/models/accounts.js
+++ src/models/accounts.js
@@ -1,25 +1,48 @@
-const mysql = require('mysql2');
-const dotenv = require('dotenv')
-dotenv.config()
+const db = require('../models/db');
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-});
+// Promisify MySQL queries
+const query = util.promisify(db.query).bind(db);
+// Function to find an account by username
+async function findOneByUsername(username) {
+  try {
+    // Query each table separately and combine the results
+    const sqlDentist = 'SELECT Dentist_ID FROM DENTIST';
+    const sqlPatient = 'SELECT PATIENT_ID FROM PATIENT';
+    const sqlStaff = 'SELECT Staff_ID FROM STAFF';
+    const [dentist] = await query(sqlDentist, [username]);
+    const [patient] = await query(sqlPatient, [username]);
+    const [staff] = await query(sqlStaff, [username]);
+    // Combine the results and return the first non-null result
+    return dentist || patient || staff || null;
+  } catch (error) {
+    throw error;
+  }
+}
 
-db.connect()
-db.query('select Dentist_ID from DENTIST', (err, result) => {
-    if (err) throw err;
-    console.log(result[0])
-})
-db.query('select Staff_ID from STAFF', (err, result) => {
-    if (err) throw err;
-    console.log(result[0])
-})
-db.query('select PATIENT_ID from PATIENT', (err, result) => {
-    if (err) throw err;
-    console.log(result[0])
-})
-db.end()
+
+const extractUsernameAndRole = (account) => {
+    const result = { username: '', role: '' };
+  
+    if (account) {
+      result.username = account.username;
+  
+      const firstTwoWords = account.username.split(' ').slice(0, 2);
+  
+      if (firstTwoWords === 'AD') {
+        result.role = 'admin';
+      } else if (firstTwoWords === 'DE') {
+        result.role = 'dentist';
+      } else if (firstTwoWords === 'ST') {
+        result.role = 'staff';
+      } else {
+        result.role = 'patient';
+      }
+    }
+  
+    return result;
+  };
+module.exports = {
+  findOneByUsername,
+  getAllUsernames,
+  extractUsernameAndRole,
+};
