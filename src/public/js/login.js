02567689d3e4739b72289e727cdc18cdffc11d0b
tgen0123 <100363182+tgen0123@users.noreply.github.com>
2023-12-04 05:12:42 +0700

    Update login.js

--- src/public/js/login.js
+++ src/public/js/login.js
@@ -5,31 +5,59 @@ $(document).ready(function () {
         e.preventDefault();
 
         const username = $('#username').val();
-        const password = "12345678";
+        const password = $('#password').val();
 
         if (!username || !password) {
             alert('Please fill in all fields');
             return;
         }
 
-        // post data to server
-        fetch('/login', {
-            method: 'POST',
+        // Make an AJAX request using the fetch API
+        fetch("/login", {
+            method: "POST",
             headers: {
-                'Content-Type': 'application/json'
+                "Content-Type": "application/json"
             },
-            body: JSON.stringify({username})
+            body: JSON.stringify({
+                username: username,
+                password: password
+            })
         })
         .then(res => {
-            if (res.redirected && res.status === 200) {
-                window.location.href = res.url;
+            if (!res.ok) {
+                throw new Error(`HTTP error! Status: ${res.status}`);
             }
-            else {
-                alert('Username or password is incorrect !');
-                throw new Error('Username or password is incorrect !!');
+            return res.json();
+        })
+        .then(account => {
+            // Handle the response data
+            console.log(account);
+
+            // Check the role from the response
+            const role = account.role;
+
+            // Redirect based on the user's role
+            if (res.redirected && res.status === 200){
+                if (role === "admin") {
+                    window.location.href = "/admin";
+                } else if (role === "dentist") {
+                    window.location.href = "/bookingAppointment";
+                } else if (role === "staff") {
+                    window.location.href = "/bookingAppointment";
+                } else if (role === "patient") {
+                    window.location.href = "/bookingAppointment";
+                } else {
+                    // Handle other roles or show an error
+                    alert("Unknown user role");
+                }
+            } else {
+                alert('Username or password is incorrect');
+                throw new Error('Username or password is incorrect');
             }
+            
         })
         .catch(error => {
+            // Handle errors during the fetch request
             console.error(error);
         });
     });
