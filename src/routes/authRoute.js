edee6a7165d8e4b83a17adc4e3df0f41b5c393eb
tgen0123 <100363182+tgen0123@users.noreply.github.com>
2023-12-05 00:25:25 +0700

    Update authRoute.js

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -3,8 +3,9 @@ const authController = require("../controllers/authController");
 
 const router = express.Router();
 
-router.get('/me', authController.Me);
-router.post('/login', authController.Login);
-router.delete('/logout', authController.Logout);
+// router.get('/me', authController.login);
+router.post('/login', authController.login);
+router.get('/login/:username', authController.verify);
+router.delete('/logout', authController.logout);
 
 module.exports = router;
