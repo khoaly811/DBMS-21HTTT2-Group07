66d078bc65a804ff31af7dac53281a12e92578a7
tgen0123 <100363182+tgen0123@users.noreply.github.com>
2023-12-05 00:25:21 +0700

    Update accountRoute.js

--- src/routes/accountRoute.js
+++ src/routes/accountRoute.js
@@ -1,30 +1,16 @@
+const accountController = require('../controllers/accountController');
+
 const express = require('express');
-const accountController = require("../controllers/accountController");
-const Auth = require("../middleware/authentication");
 const router = express.Router();
 
-router.get('/accounts', Auth.verifyAccount, Auth.adminOnly, accountController.getAccounts);
-router.get('/accounts/:id', Auth.verifyAccount, Auth.adminOnly, accountController.getAccountById);
-router.post('/accounts', Auth.verifyAccount, Auth.adminOnly, accountController.createAccount);
-router.patch('/accounts/:id', Auth.verifyAccount, Auth.adminOnly, accountController.updateAccount);
-router.delete('/accounts/:id', Auth.verifyAccount, Auth.adminOnly, accountController.deleteAccount);
-
-router.get('/accounts', Auth.verifyAccount, Auth.dentistOnly, accountController.getAccounts);
-router.get('/accounts/:id', Auth.verifyAccount, Auth.dentistOnly, accountController.getAccountById);
-router.post('/accounts', Auth.verifyAccount, Auth.dentistOnly, accountController.createAccount);
-router.patch('/accounts/:id', Auth.verifyAccount, Auth.dentistOnly, accountController.updateAccount);
-router.delete('/accounts/:id', Auth.verifyAccount, Auth.dentistOnly, accountController.deleteAccount);
-
-router.get('/accounts', Auth.verifyAccount, Auth.staffOnly, accountController.getAccounts);
-router.get('/accounts/:id', Auth.verifyAccount, Auth.staffOnly, accountController.getAccountById);
-router.post('/accounts', Auth.verifyAccount, Auth.staffOnly, accountController.createAccount);
-router.patch('/accounts/:id', Auth.verifyAccount, Auth.staffOnly, accountController.updateAccount);
-router.delete('/accounts/:id', Auth.verifyAccount, Auth.staffOnly, accountController.deleteAccount);
-
-router.get('/accounts', Auth.verifyAccount, Auth.patientOnly, accountController.getAccounts);
-router.get('/accounts/:id', Auth.verifyAccount, Auth.patientOnly, accountController.getAccountById);
-router.post('/accounts', Auth.verifyAccount, Auth.patientOnly, accountController.createAccount);
-router.patch('/accounts/:id', Auth.verifyAccount, Auth.patientOnly, accountController.updateAccount);
+router.get('/account', accountController.findAll);
+router.get('/account/:username', accountController.findOne);
+router.patch('/account/:username', accountController.update);
+router.delete('/account/:username', accountController.delete);
 
 
-module.exports =  router;
+router.post('/signup', accountController.create);
+
+module.exports = router;
+
+
