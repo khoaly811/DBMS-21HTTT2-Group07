64f7b0f56b96169eb1ff4bd7c51c788f686d7d14
tgen0123 <100363182+tgen0123@users.noreply.github.com>
2023-12-06 15:59:58 +0700

    Update procedure.js

--- src/models/procedure.js
+++ src/models/procedure.js
@@ -66,7 +66,7 @@
 
 // delimiter //
 
-// create procedure sp_get_dentist(in clinicID int, in wishDate char(1), in shift int)
+// create procedure sp_get_dentist(in clinicID char(5), in wishDate char(1), in shift int)
 // begin
 // 	select distinct D.dentist_id, D.full_name
 // 	from (select * from dentist where clinic_id = clinicID) as D
